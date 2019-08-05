667c64c55f56ae3b95acc7e3b856ec15c7496365
ghdmsrkd <ghdmsrkd@naver.com>
2019-08-06 00:19:38 +0900

    add get customers rest api module

--- client/src/App.js
+++ client/src/App.js
@@ -22,35 +22,24 @@ const styles = theme =>({
   }
 })
 
-
-const customers = [
-  {
-  'id':1,
-  'image': 'http://placeimg.com/64/64/1',
-  'name':'강은호',
-  'birthday':'970817',
-  'gender':'남자',
-  'job':'대학생'
-   },
-  {
-  'id':2,
-  'image': 'http://placeimg.com/64/64/2',
-  'name':'강은호2',
-  'birthday':'970817',
-  'gender':'남자',
-  'job':'대학생'
-   },
-  {
-   'id':3,
-   'image': 'http://placeimg.com/64/64/3',
-   'name':'강은호3',
-   'birthday':'970817',
-   'gender':'남자',
-   'job':'대학생'
-  }
-]
-
 class App extends Component {
+
+state ={
+  customers:""
+}
+
+componentDidMount(){
+  this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+}
+
+callApi = async () => {
+  const response = await fetch('/api//customers');
+  const body =await response.json();
+  return body;
+}
+
   render(){
     const {classes} = this.props;
     return (
@@ -67,7 +56,9 @@ class App extends Component {
             </TableRow>
           </TableHead>
         <TableBody>
-        {customers.map(c =>{return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday}gender={c.gender}job={c.job} />)})}
+          {this.state.customers ? 
+          this.state.customers.map(c =>{return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday}gender={c.gender}job={c.job} />) 
+          }) : ""}
         </TableBody>
         </Table>
 
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT ||5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
-});
-app.listen
+app.get('/api/customers', (req,res) => {
+    res.send([
+        {
+        'id':1,
+        'image': 'http://placeimg.com/64/64/1',
+        'name':'강은호',
+        'birthday':'970817',
+        'gender':'남자',
+        'job':'대학생'
+         },
+        {
+        'id':2,
+        'image': 'http://placeimg.com/64/64/2',
+        'name':'강은호2',
+        'birthday':'970817',
+        'gender':'남자',
+        'job':'대학생'
+         },
+        {
+         'id':3,
+         'image': 'http://placeimg.com/64/64/3',
+         'name':'강은호3',
+         'birthday':'970817',
+         'gender':'남자',
+         'job':'대학생'
+        }
+      ]);
+})
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
